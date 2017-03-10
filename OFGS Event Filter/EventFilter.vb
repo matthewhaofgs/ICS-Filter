c71da4364ba713fddf6959af860143887ebb801d
matthewhaofgs <matthewha@ofgs.nsw.edu.au>
2017-03-10 14:46:23 +1100

    Dev

--- OFGS Event Filter/EventFilter.vb
+++ OFGS Event Filter/EventFilter.vb
@@ -1,15 +1,6 @@
-﻿Public Class EventFilter
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+﻿Imports System.IO
 
-    End Sub
-
-    Private Sub FolderBrowserDialog1_HelpRequest(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
-
-    End Sub
+Public Class EventFilter
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim fd As OpenFileDialog = New OpenFileDialog()
@@ -23,18 +14,6 @@
         If fd.ShowDialog() = DialogResult.OK Then
             TextBox1.Text = fd.FileName
         End If
-    End Sub
-
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
-
-    End Sub
-
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-
-    End Sub
-
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
@@ -53,13 +32,73 @@
 
     Sub main()
 
+        Dim ICSEvents As List(Of List(Of String))
+        ICSEvents = readICS(TextBox1.Text)
+
+        Dim csvEvents As List(Of String())
+        csvEvents = readCSV(TextBox2.Text)
+
+
+
+
     End Sub
 
     Function readICS(filename As String)
+        Dim ICSEvents As New List(Of List(Of String))
+        ICSEvents.Add(New List(Of String))
+        Try
+            Using sr As New StreamReader(filename)
+                Dim line As String
+                While Not sr.EndOfStream
+                    line = sr.ReadLine
 
+                    If line.Contains("BEGIN:VEVENT") Then
+                        ICSEvents.Add(New List(Of String))
+                    End If
+                    ICSEvents.Last.Add(line)
+                End While
+            End Using
+        Catch e As Exception
+            MsgBox(e.Message)
+        End Try
+        Return ICSEvents
     End Function
 
     Function readCSV(filename As String)
+        Dim csvText As New List(Of String)
+        Dim csvEvents As New List(Of String())
+
+        Try
+            Using sr As New StreamReader(filename)
+                Dim line As String
+                While Not sr.EndOfStream
+                    line = sr.ReadLine
+                    csvText.Add(line)
+                End While
+            End Using
+        Catch e As Exception
+            MsgBox(e.Message)
+        End Try
+
+        For Each eventLine In csvText
+            csvEvents.Add(Split(eventLine, ","))
+        Next
+        Return csvEvents
+    End Function
+
+    Function filterEvents(ICSEvents As List(Of List(Of String)), csvEvents As List(Of String()))
+        For Each ICSEvent In ICSEvents
+            For Each eventLine In ICSEvent
+                If eventLine.Contains("") Then
+
+                End If
+            Next
+        Next
+
+
+    End Function
+
+    Function getEventsToPublish()
 
     End Function
 
